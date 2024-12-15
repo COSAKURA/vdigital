aa979f6b85cb0b170718a03a4ea6f79fd6936ac1
Sakura <githup147@outlook.com>
2024-12-15 13:05:33 +0800

    c

--- src/views/AilView.vue
+++ src/views/AilView.vue
@@ -60,7 +60,7 @@ export default {
       this.messages.push({ from: "user", text: this.newMessage });
 
       try {
-        const response = await fetch("http://127.0.0.1:11434/api/chat", {
+        const response = await fetch("http://172.46.225.96:11434/api/chat", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
